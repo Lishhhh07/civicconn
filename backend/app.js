d0c6b4d490ddb46801a36b49540a44d30fa45869
lishhh06 <lishikameghani@gmail.com>
2026-04-14 01:08:44 +0530

    cors fix

--- backend/app.js
+++ backend/app.js
@@ -9,11 +9,9 @@ import { loggerMiddleware } from "./middleware/loggerMiddleware.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN?.split(",") || "*"
-  })
-);
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
